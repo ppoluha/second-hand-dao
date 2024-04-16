c50e09fe9fc5fb5c5ccd23b0be0cb198fd0bf2eb
Pelle Poluha <pelle@poluha.se>
2024-04-16 09:37:26 +0200

    Makes it a Maven project

--- src/main/java/se/hkr/secondhand/Main.java
+++ src/main/java/se/hkr/secondhand/Main.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (Connection conn = DBConnection.getConnection()) {
             EmployeeDAO employeeDAO = new EmployeeDAO(conn);
             OrderDAO orderDAO = new OrderDAO(conn);
@@ -24,15 +24,15 @@ public class Main {
             employeeDAO.createEmployee(newEmployee);
 
             // Example: Create a new order
-            OrderHead newOrder = new OrderHead(4, LocalDate.now(), 1, 1);
+            OrderHead newOrder = new OrderHead(-1, LocalDate.now(), 1, 1);
             List<OrderLine> lines = new ArrayList<>();
-            lines.add(new OrderLine(10, 1, 4, 2));
+            lines.add(new OrderLine(-1, 1, -1, 3));
             orderDAO.createOrder(newOrder, lines);
 
             // Display all employees
             List<Employee> employees = employeeDAO.listAllEmployees();
             employees.forEach(System.out::println);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
     }
